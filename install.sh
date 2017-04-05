d3e5067dc60b827077576ae19a4e6297e036a72b
Josh Hollandsworth <josh.hollandsworth+dev@gmail.com>
2017-04-04 20:32:20 -0500

    added docker to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 ## Install the things
-sudo apt-get install -y git neovim stow zsh curl tmux
+sudo apt-get install -y \
+	git \
+	neovim \
+	stow \
+	zsh \
+	curl \
+	tmux \
+	apt-transport-https \
+	ca-certificates \
+	software-properties-common
 
 if test -n "$(find $HOME/.local/share/fonts/ -maxdepth 1 -name '*Powerline*' -print -quit)"
 then
@@ -28,3 +37,15 @@ mkdir $HOME/Source
 
 git clone https://github.com/joshholl/dotfiles $HOME/Source/dotfiles
 
+## Install docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/debian \
+	$(lsb_release -cs) \
+	stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo systemctl start docker
