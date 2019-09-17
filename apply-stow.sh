10a2526910909544976ee4bb4f9dce4ec481f637
Josh Hollandsworth <josh@joshholl.com>
2019-09-17 18:31:01 -0500

    making apply stow execute a post install step

--- apply-stow.sh
+++ apply-stow.sh
@@ -3,7 +3,7 @@
 has_stow () {
 	if [ ! -f "$(command -v stow)" ]; then
 		echo ' You do not have gnu stow install '
-		exit 1;
+		exit 1
 	else 
 		return 0
 	fi
@@ -13,19 +13,29 @@ rename_old_profile () {
 	[ -f $HOME/.profile ] && ! [ -L $HOME/.profile ]  && mv $HOME/.profile $HOME/.profile_pre_stow
 }
 
-
 make_required_dirs () {
 	mkdir -p $(pyenv root)/plugins
 	mkdir -p $(rbenv root)/plugins
 }
 
+rehydrate_sub_modules () {
+	git submodule init
+	git submodule update
+}
+apply () {
+	stow -R -t ~/ files
+	stow -R -t $(pyenv root)/plugins pyenv-plugins
+	stow -R -t $(rbenv root)/plugins rbenv-plugins
+}
+
+execute_post_stow () {
+	find ./scripts -maxdepth 1 -type f -name '*.sh' -exec {} \; > results.out
+}
+
 has_stow
 rename_old_profile
 make_required_dirs
+rehydrate_sub_modules
+apply
+execute_post_stow
 
-
-git submodule init
-git submodule update
-stow -R -t ~/ files
-stow -R -t $(pyenv root)/plugins pyenv-plugins
-stow -R -t $(rbenv root)/plugins rbenv-plugins
