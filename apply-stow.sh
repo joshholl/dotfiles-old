964071ba39a18d675be58ced7aa55fe6f38590e2
Josh Hollandsworth <josh@joshholl.com>
2019-09-17 18:19:02 -0500

    adding check to ensure stow is present

--- apply-stow.sh
+++ apply-stow.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/env bash
+
+has_stow () {
+	if [ ! -f "$(command -v stow)" ]; then
+		echo ' You do not have gnu stow install '
+		exit 1;
+	else 
+		return 0
+	fi
+}
+
+
+
+has_stow
 git submodule init
 git submodule update
 stow -R -t ~/ files
